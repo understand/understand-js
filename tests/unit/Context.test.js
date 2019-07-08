1c664b49bedd6531a09dd34c8df5e6d0205ba18a
Maurizio Bonani <maurizio.bonani@gmail.com>
2019-07-08 16:47:27 +0200

    Rework through context api

--- tests/unit/Context.test.js
+++ tests/unit/Context.test.js
@@ -6,89 +6,194 @@ let event;
 
 describe('Context', () => {
   beforeEach(() => {
+    sessionStorage.clear();
+
     context = new Context();
 
     event = {};
   });
 
-  describe('session', () => {
-    test('it should add session details', () => {
-      const session = { id: '123456' };
-
-      context.setSession(session);
-
-      expect(context.session).toEqual(session);
-    });
-
-    test('it should map `id` to `session_id` when applying context to event', () => {
-      const session = { id: '123456' };
-
-      context.setSession(session);
-
-      expect(context.applyToEvent(event)).toEqual({
+  describe.only('session', () => {
+    test('it should add session id provided by the user via constructor', () => {
+      context = new Context({
         session_id: '123456'
       });
+
+      expect(context.getSessionId()).toEqual('123456');
     });
 
-    test('it should not add `session_id` to event if it is not a valid primitive value', () => {
+    test('it should add session id provided by the user via setter', () => {
+      const session_id = '123456';
+
+      context.setSessionId(session_id);
+
+      expect(context.getSessionId()).toEqual(session_id);
+    });
+
+    test('it should throw an error when setting an invalid value as session id', () => {
       [null, undefined, NaN, '', 0].forEach(val => {
-        event = {};
-        let session = { id: val };
-
-        context.setSession(session);
-
-        expect(context.applyToEvent(event)).toEqual({});
+        expect(() => {
+          context.setSessionId(val)
+        }).toThrow();
       });
+    });
+
+    test('it should apply the session_id to the event when applying context', () => {
+      const session_id = '123456';
+
+      context.setSessionId(session_id);
+
+      expect(context.applyToEvent(event)).toMatchObject({
+        session_id
+      });
+    });
+
+    test('it should return a default session id value if not provided by the user', () => {
+      const session_id = context.getSessionId();
+
+      expect(typeof session_id).toEqual('string');
+      expect(session_id.length).toEqual(40);
+      expect(session_id).toEqual(context.getSessionId());
+    });
+
+    test('it should store the default session id value in session storage', () => {
+      const session_id = context.getSessionId();
+
+      expect(sessionStorage.setItem).toHaveBeenCalledTimes(1);
+      expect(Object.keys(sessionStorage.__STORE__).length).toBe(1);
+      expect(JSON.parse(sessionStorage.getItem('understand_session_id')).session_id).toEqual(session_id);
+    });
+
+    test('it should refresh the session id value stored in session storage if it is older than 2 hours', () => {
+      const session_id = context.getSessionId();
+
+      sessionStorage.__STORE__['understand_session_id'] = JSON.stringify({
+        expire_at: new Date(new Date().getTime() - (1000 * 60 * 60 * 2)),
+        session_id
+      });
+
+      expect(context.getSessionId()).not.toEqual(session_id);
+    });
+
+    test('it should add request id provided by the user via constructor', () => {
+      context = new Context({
+        request_id: '08394443-31d5-4d65-8392-a29d733c498d'
+      });
+
+      expect(context.getRequestId()).toEqual('08394443-31d5-4d65-8392-a29d733c498d');
+    });
+
+    test('it should add request id provided by the user via setter', () => {
+      const request_id = '08394443-31d5-4d65-8392-a29d733c498d';
+
+      context.setSessionId(request_id);
+
+      expect(context.getSessionId()).toEqual(request_id);
+    });
+
+    test('it should throw an error when setting an invalid value as request id', () => {
+      [null, undefined, NaN, '', 0].forEach(val => {
+        expect(() => {
+          context.setRequestId(val)
+        }).toThrow();
+      });
+    });
+
+     test('it should return a default request id value if not provided by the user', () => {
+      const request_id = context.getRequestId();
+
+      expect(typeof request_id).toEqual('string');
+      expect(request_id.length).toEqual(36); // uuid v4
+
+      expect(request_id).not.toEqual(context.getRequestId());
     });
   });
 
   describe('user', () => {
-    test('it should add user details', () => {
-      const user = {
-        id: 1,
-        ip_address: '192.168.10.10'
-      };
-
-      context.setUser(user);
-
-      expect(context.user).toEqual(user);
-    });
-
-    test('it should map `id` to `user_id` when applying context to event', () => {
-      const user = {
-        id: 1
-      };
-
-      context.setUser(user);
-
-      expect(context.applyToEvent(event)).toEqual({
+    test('it should add user id provided by the user via constructor', () => {
+      context = new Context({
         user_id: 1
       });
+
+      expect(context.user.user_id).toEqual(1);
     });
 
-    test('it should map `ip_address` to `client_ip` when applying context to event', () => {
-      const user = {
-        ip_address: '192.168.10.10'
-      };
+    test('it should add user id provided by the user via setter', () => {
+      const user_id = 1;
 
-      context.setUser(user);
+      context.setUserId(user_id);
 
-      expect(context.applyToEvent(event)).toEqual({
-        client_ip: '192.168.10.10'
+      expect(context.user.user_id).toEqual(user_id);
+    });
+
+    test('it should apply `user_id` to the event when applying context', () => {
+      const user_id = 1;
+
+      context.setUserId(user_id);
+
+      expect(context.applyToEvent(event)).toMatchObject({
+        user_id
       });
     });
 
-    test('it should not add `user_id` or `client_ip` to event if it is not a valid primitive value', () => {
+    test('it should clear the session id value in local storage if session id is not provided', () => {
+      const user_id = 1;
+
+      context.setUserId(user_id);
+
+      expect(sessionStorage.removeItem).toHaveBeenCalledTimes(1);
+      expect(Object.keys(sessionStorage.__STORE__).length).toBe(0);
+    });
+
+    test('it should not clear the session id value in local storage if session id is already provided', () => {
+      const user_id = 1;
+
+      context.setSessionId('123456');
+      context.setUserId(user_id);
+
+      expect(sessionStorage.removeItem).not.toHaveBeenCalled();
+      expect(Object.keys(sessionStorage.__STORE__).length).toBe(0);
+    });
+
+    test('it should throw an error when setting an invalid value as user id', () => {
       [null, undefined, NaN, '', 0].forEach(val => {
-        event = {};
-        let user = {
-          id: val,
-          ip_address: val
-        };
+        expect(() => {
+          context.setUserId(val)
+        }).toThrow();
+      });
+    });
 
-        context.setUser(user);
+    test('it should add client ip provided by the user via constructor', () => {
+      context = new Context({
+        client_ip: '192.168.10.10'
+      });
 
-        expect(context.applyToEvent(event)).toEqual({});
+      expect(context.user.client_ip).toEqual('192.168.10.10');
+    });
+
+    test('it should add client ip provided by the user via setter', () => {
+      const client_ip = '192.168.10.10';
+
+      context.setClientIp(client_ip);
+
+      expect(context.user.client_ip).toEqual(client_ip);
+    });
+
+    test('it should apply `client_ip` to the event when applying context', () => {
+      const client_ip = '192.168.10.10';
+
+      context.setClientIp(client_ip);
+
+      expect(context.applyToEvent(event)).toMatchObject({
+        client_ip
+      });
+    });
+
+    test('it should throw an error when setting an invalid value as client ip', () => {
+      [null, undefined, NaN, '', 0].forEach(val => {
+        expect(() => {
+          context.setClientIp(val)
+        }).toThrow();
       });
     });
   });
@@ -107,7 +212,7 @@ describe('Context', () => {
 
       context.setTags(tags);
 
-      expect(context.applyToEvent(event)).toEqual({
+      expect(context.applyToEvent(event)).toMatchObject({
         tags
       });
     });
@@ -127,7 +232,7 @@ describe('Context', () => {
 
       context.setTags(tags);
 
-      expect(context.applyToEvent(event)).toEqual({
+      expect(context.applyToEvent(event)).toMatchObject({
         tags: []
       });
     });
@@ -137,7 +242,7 @@ describe('Context', () => {
 
       context.setTags(tags);
 
-      expect(context.applyToEvent(event)).toEqual({
+      expect(context.applyToEvent(event)).toMatchObject({
         tags: ['one', 'two', 'three']
       });
     });
@@ -195,19 +300,41 @@ describe('Context', () => {
     });
   });
 
-  test('it should clear context', () => {
-    const session = { id: '123456' };
-    const tags = ['one', 'two', 'three'];
-    const user = { id: 1, ip_address: '192.168.10.10' };
+  test('it should initialize context via JS object', () => {
+    context = new Context({
+      'session_id': '123456',
+      'request_id': '08394443-31d5-4d65-8392-a29d733c498d',
+      'user_id': 1,
+      'client_ip': '192.168.10.10',
+      'tags': ['one', 'two', 'three']
+    });
 
-    context.setSession(session);
-    context.setTags(tags);
-    context.setUser(user);
+    expect(context.session).toMatchObject({
+      'session_id': '123456',
+      'request_id': '08394443-31d5-4d65-8392-a29d733c498d'
+    });
+
+    expect(context.user).toMatchObject({
+      'user_id': 1,
+      'client_ip': '192.168.10.10'
+    });
+
+    expect(context.tags).toEqual(['one', 'two', 'three']);
+  });
+
+  test('it should clear context', () => {
+    context.setSessionId('123456');
+    context.setTags(['one', 'two', 'three']);
+    context.setUserId(1);
+    context.setClientIp('192.168.10.10');
 
     context.clear();
 
     expect(context.session).toEqual({});
     expect(context.tags).toEqual([]);
     expect(context.user).toEqual({});
+
+    expect(sessionStorage.removeItem).toHaveBeenCalledTimes(1);
+    expect(Object.keys(sessionStorage.__STORE__).length).toBe(0);
   });
 });
